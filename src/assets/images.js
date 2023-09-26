a9097ef6c517b44b2712510c03a7a6999b2addd6
Jummate <yakubjumat@gmail.com>
2023-09-26 22:40:50 +0100

    refactor: reword the description of projects

--- src/assets/images.js
+++ src/assets/images.js
@@ -3,12 +3,15 @@ import Converter from "./currency-converter.png";
 import GuessNinja from "./guess-ninja.png";
 import Bricademia from "./bricademia.png";
 import MyPhoto from "./my-photo.jpg";
+// import MyPhoto2 from "./my-photo2.jpg";
+// import MyPhoto3 from "./my-photo3.jpg";
 import CAMS from "./cams.png";
 
 export const images = {
   Marketeer,
   Converter,
   MyPhoto,
+  // MyPhoto3,
   GuessNinja,
   Bricademia,
   CAMS,
