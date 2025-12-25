22d9ef0eeca93e8148e2cc27ca1592ee383c3eef
Jummate <yakubjumat@gmail.com>
2025-12-25 10:12:14 +0100

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css';
+
+function App() {
+   return (
+      <>
+         <div className="text-red-400">Hello world</div>
+         <div>Hello world 2</div>
+      </>
+   );
+}
+
+export default App;
